b238d19bdd870f47b6a87ea5edbeef232e103355
matias-gonz <maigonzalez@fi.uba.ar>
2021-04-30 20:10:54 -0300

    Agrego base de clase Platform

---
+++ src/model/Platform.cpp
@@ -0,0 +1,6 @@
+#include "Platform.h"
+
+Platform::Platform(int pos, int amount) {
+    myPos = pos;
+    myAmount = amount;
+}
---
+++ src/model/Platform.h
@@ -0,0 +1,15 @@
+#ifndef TALLER_PROG_I_2021_1C_KIWI_PLATFORM_H
+#define TALLER_PROG_I_2021_1C_KIWI_PLATFORM_H
+
+
+class Platform {
+private:
+    Position myPos;
+    int myAmount;
+
+public:
+    Platform(Position pos, int amount);
+};
+
+
+#endif //TALLER_PROG_I_2021_1C_KIWI_PLATFORM_H
