ef47d3628ebe0d20921791ce3c6f9fd5cf22dd63
Matías Ignacio González <74241889+matias-gonz@users.noreply.github.com>
2021-05-01 20:22:42 -0300

    update (#21)
    * Fix issues with position
    
    * Add cmake file with basic window in sdl
    
    * Mejorado el codigo del main loop del programa
    
    * Añadido el método add a position.
    Add recibe dos parametros, que vienen a ser lo que se le va a sumar a cada variable de la posicion actual.
    
    * Añadidas unos metodos del personaje
    
    * Añadidos algunos sprites a resources
    
    Co-authored-by: Nico Hazan <nicohaza@gmail.com>
    Co-authored-by: haaza23 <51170462+haaza23@users.noreply.github.com>
    Co-authored-by: jpdicomo <jdicomo@fi.uba.ar>
    Co-authored-by: jpdicomo <39575121+jpdicomo@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -2,46 +2,60 @@
 #include "SDL2/SDL_image.h"
 #include <stdio.h>
 
-const int SCREEN_WIDTH = 1280;
+
+bool running;
+const int SCREEN_WIDTH = 980;
+
 const int SCREEN_HEIGHT = 720;
 
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 
-void renderImage() {
-  //Initialize SDL
-  if(SDL_Init( SDL_INIT_VIDEO ) < 0) {
-    printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-  } else {
-    //Create window
-    window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                              SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    renderer = SDL_CreateRenderer(window, -1, 0);
+
+void initializeScreen(){
+    //Initialize SDL
+    if(SDL_Init( SDL_INIT_VIDEO ) < 0) {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    } else {
+        //Create window
+        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                                  SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        renderer = SDL_CreateRenderer(window, -1, 0);
+        SDL_SetRenderDrawColor(renderer, 255, 128, 128, 255);
+
+    }
+}
+void render() {
     SDL_SetRenderDrawColor(renderer, 255, 128, 128, 255);
     SDL_RenderClear(renderer);
-  }
+    SDL_RenderPresent(renderer);
+
 }
 
-void closeWindow() {
-  SDL_Event event;      //Event handler
 
-  while(event.type != SDL_QUIT){
-    while(SDL_PollEvent(&event)){
-      if (event.type == SDL_QUIT){
-        SDL_Quit();
-        exit(1);
-      }
-      SDL_RenderPresent(renderer);
-      // to simulate OP's flashingText()
-      SDL_Delay(50);
-    }
-    SDL_Delay(100);
-  }
+void update(){
+
 }
-
 int main( int argc, char* args[] ) {
-  renderImage();
-  closeWindow();
+    running = true;
+    initializeScreen();
 
-  return 0;
+    SDL_Event event;
+
+    while (running) {
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                SDL_Quit();
+                running = false;
+            }
+            update();
+            render();
+        }
+        SDL_Delay(1000/60);
+    }
+    SDL_Quit();
+
+    return 0;
+
 }
--- src/model/mario.cpp
+++ src/model/mario.cpp
@@ -1,8 +1,25 @@
 
 
 #include "mario.h"
-class Mario{
-    bool seCreoBien(){
-        return true;
+#include "Position.h"
+
+Mario::Mario(double xInit, double yInit): pos(xInit,yInit){
+    pos = Position(xInit, yInit);
+
+}
+
+void Mario::moveLeft() {
+    pos.add(-10,0);
+}
+
+void Mario::moveRight() {
+    pos.add(10,0);
+}
+
+void Mario::jumpUp() {
+    if (isGrounded){
+        return;
     }
-};
+}
+
+
