f2c6ee47e340977beafc11d695f52a5b0e8ebfbc
jpdicomo <jdicomo@fi.uba.ar>
2021-05-05 15:28:18 -0300

    Añadida a funcion loadImages que carga las imagenes en las varibables globales. Y cambiada la forma en que se inicial el juego, teniendo que se crea el "game" luego se cargan las imagenes, luego el player con su textura y finalmente se asigna el player a el "game"

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include "src/model/Game.h"
+#include "src/view/TextureLoader.h"
 
 Game* game = NULL;
 
-int main( int argc, char* args[] ) {
+void loadImages();
+
+SDL_Texture* playerTexture;
+
+
+int main(int argc, char* args[] ) {
 
     game = new Game();
     game->init("Donkey Kong",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600,false);
+
+    loadImages();
+    if (playerTexture == NULL){
+        printf("%s",SDL_GetError());
+    }
+
+    Player player = Player(playerTexture);
+    game->player = &player;
 
     while(game->running()){
         game->handleEvents();
@@ -15,4 +29,8 @@ int main( int argc, char* args[] ) {
     game->clean();
 
     return 0;
+}
+
+void loadImages() {
+    playerTexture = TextureLoader::loadTexture("/home/jay/Desktop/Taller/Taller-Prog-I-2021-1C-KIWI/resources/sprites/sans_left.png");
 }
