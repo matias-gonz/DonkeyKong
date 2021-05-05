543d704c5ed6c6b0b371c96b6c8f888de9d2672d
jpdicomo <jdicomo@fi.uba.ar>
2021-05-05 15:29:13 -0300

    Añadidido un checkeo de que se haya cargado bien la imagen

--- src/view/TextureLoader.cpp
+++ src/view/TextureLoader.cpp
@@ -3,6 +3,7 @@
 
 SDL_Texture* TextureLoader::loadTexture(const char *file) {
     SDL_Surface* tmpSurface = IMG_Load(file);
+    if (tmpSurface==NULL){ printf("%s",SDL_GetError());}
     SDL_Texture* texture = SDL_CreateTextureFromSurface(Game::renderer,tmpSurface);
     SDL_FreeSurface(tmpSurface);
     return texture;
