cbf33041979152788cdc16075a1229a7a491b84b
rociotarda <rtarda@fi.uba.ar>
2021-05-18 22:49:06 -0300

    detalles

--- src/model/Barrel.cpp
+++ src/model/Barrel.cpp
@@ -6,10 +6,6 @@ Barrel::Barrel()
     //Initialize the offsets
     //posX = 200;
     //posY = 0;
-
-    //Initialize the velocity
-    //velX = 1;
-    //velY = 1;
 }
 Barrel::Barrel(Position position, int count, int direction){
     this->srcRect = NULL;
@@ -45,37 +41,3 @@ SDL_Rect *Barrel::getSrcRect() {
 int Barrel::getCount() {
     return count;
 }
-
-
-
-
-/*
-Barrel::Barrel(LTexture bTexture)
-{
-    //Initialize the offsets
-    posX = 200;
-    posY = 0;
-
-    //Initialize the velocity
-    velX = 1;
-    velY = 1;
-    this->barrelTexture=bTexture;
-}
-
-void Barrel::move()
-{
-    //Move the dot down
-    posY += velY;
-}
-
-void Barrel::render()
-{
-    //Show the dot
-    barrelTexture.render(posX, posY );
-}
-
-void Barrel:: setTexture(LTexture* bTexture){
-    this->barrelTexture = *bTexture;
-}
-
-*/
