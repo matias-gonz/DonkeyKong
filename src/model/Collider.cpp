932614f9a64c8b42349f22ed54b06b6c59f9b355
matias-gonz <maigonzalez@fi.uba.ar>
2021-05-25 12:33:50 -0300

    Added Collider class

---
+++ src/model/Collider.cpp
@@ -0,0 +1,5 @@
+//
+// Created by matiti on 5/25/21.
+//
+
+#include "Collider.h"
---
+++ src/model/Collider.h
@@ -0,0 +1,14 @@
+//
+// Created by matiti on 5/25/21.
+//
+
+#ifndef TALLER_PROG_I_2021_1C_KIWI_COLLIDER_H
+#define TALLER_PROG_I_2021_1C_KIWI_COLLIDER_H
+
+
+class Collider {
+
+};
+
+
+#endif //TALLER_PROG_I_2021_1C_KIWI_COLLIDER_H
