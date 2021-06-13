005226232e6a53fe2a48272316c56729925ca655
matias-gonz <maigonzalez@fi.uba.ar>
2021-06-13 20:44:27 -0300

    Added thread safe queue

---
+++ src/controller/QueueThrd.cpp
@@ -0,0 +1,25 @@
+#include <SDL_events.h>
+#include "QueueThrd.h"
+#include <pthread.h>
+
+QueueThrd::QueueThrd() {
+  pthread_mutex_init(&this->mutex,NULL);
+  this->queue = std::queue<SDL_Event>();
+}
+
+void QueueThrd::push(SDL_Event e) {
+  pthread_mutex_lock(&this->mutex);
+  this->queue.push(e);
+  pthread_mutex_unlock(&this->mutex);
+}
+
+SDL_Event QueueThrd::pop(){
+  pthread_mutex_lock(&this->mutex);
+  SDL_Event e = this->queue.front();
+  pthread_mutex_unlock(&this->mutex);
+  return e;
+}
+
+bool QueueThrd::isEmpty() {
+  return this->queue.empty();
+}
---
+++ src/controller/QueueThrd.h
@@ -0,0 +1,18 @@
+#ifndef TALLER_PROG_I_2021_1C_KIWI_QUEUETHRD_H
+#define TALLER_PROG_I_2021_1C_KIWI_QUEUETHRD_H
+#include<queue>
+
+class QueueThrd {
+public:
+    QueueThrd();
+
+    void push(SDL_Event e);
+    SDL_Event pop();
+    bool isEmpty();
+private:
+    std::queue<SDL_Event> queue;
+    pthread_mutex_t mutex;
+};
+
+
+#endif //TALLER_PROG_I_2021_1C_KIWI_QUEUETHRD_H
